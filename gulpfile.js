4e2d8e1d10f0f81520163770c39ec69729f1f5ec
Laura gonzalez <lawr@lauragonzalez.cc>
2015-08-02 14:27:57 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var babelify = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('modules', function() {
+    browserify({
+	    entries: './wavelert.js',
+	    debug: true
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('wavelert.js'))
+    .pipe(gulp.dest('./dist'));
+});
