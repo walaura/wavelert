dc9fee852dad75fb44aefa519b523484552bd764
Lau Skeeter <lawr@lauragonzalez.cc>
2019-03-03 20:01:57 +0000

    gh pages

--- gh-pages/src.a2b27638.js
+++ gh-pages/src.a2b27638.js
@@ -630,7 +630,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65064" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49783" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
