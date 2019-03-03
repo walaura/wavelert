19901252589ecdc28fe1e84a3e0ec864f493645b
Lau Skeeter <lawr@lauragonzalez.cc>
2019-03-03 19:35:56 +0000

    cleanup

--- src/index.js
+++ src/index.js
@@ -3,41 +3,34 @@ import { Dialog } from './global/Dialog';
 import { Alert } from './global/Alert';
 import { Confirm } from './global/Confirm';
 
-const withCloseWrapper = fn => {
-	return {
-		place: () =>
-			new Promise((yay, nay) => {
-				const onClose = state => {
-					$item.remove();
-					state ? yay() : nay();
-				};
-				const $item = fn(onClose);
-				document.body.appendChild($item);
-				if ($item.activate) {
-					$item.activate();
-				}
-			}),
-	};
-};
+const withCloseWrapper = fn => () =>
+	new Promise((yay, nay) => {
+		const onClose = state => {
+			$item.remove();
+			state ? yay() : nay();
+		};
+		const $item = fn(onClose);
+		document.body.appendChild($item);
+		if ($item.activate) {
+			$item.activate();
+		}
+	});
 
 const promisified = {
-	Alert: ({ ...props }) => {
-		return withCloseWrapper(closeWrapper =>
+	Alert: ({ ...props }) =>
+		withCloseWrapper(closeWrapper =>
 			WaveWindow(
 				{ ...props, onClose: () => closeWrapper(false) },
 				Alert({ ...props, onClose: closeWrapper })
 			)
-		);
-	},
-
-	Confirm: ({ ...props }) => {
-		return withCloseWrapper(closeWrapper =>
+		),
+	Confirm: ({ ...props }) =>
+		withCloseWrapper(closeWrapper =>
 			WaveWindow(
 				{ ...props, onClose: () => closeWrapper(false) },
 				Confirm({ ...props, onClose: closeWrapper })
 			)
-		);
-	},
+		),
 };
 
 export { promisified, Alert, Confirm, Dialog, WaveWindow };
